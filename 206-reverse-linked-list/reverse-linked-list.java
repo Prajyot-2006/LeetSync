138d4bf154fc3f92a2b09ea204e8ecc4da595e26
Prajyot Jore <prajyotjore06@gmail.com>
2026-09-22 15:51:42 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (30.87%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ArrayList<ListNode> arr = new ArrayList<>();
+        ListNode temp = head;
+        while(temp!=null) {
+            arr.add(temp);
+            temp = temp.next;
+        }
+        int n = arr.size();
+        for(int i=n-1;i>0;i--) {
+            ListNode t1 = arr.get(i);
+            ListNode t2 = arr.get(i-1);
+            t1.next = t2;
+        }
+        if(head==null || head.next==null) return head;
+        arr.get(0).next = null;
+        return arr.get(n-1);
+    }
+}
